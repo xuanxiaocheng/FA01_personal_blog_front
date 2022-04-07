eec5cba6b0ae6f090cdd5c83def0a9d2e593cc51
xuanxiaocheng <2313821795@qq.com>
2022-04-08 00:35:29 +0800

    gitignore db.js

--- express/server/db.js
+++ express/server/db.js
@@ -1,8 +1,8 @@
 module.exports = {
     mysql: {
-        host: '...',
-        user: '...',
-        password: '...',
+        host: 'rm-uf6968rw678931y5zko.mysql.rds.aliyuncs.com',
+        user: 'mysql123',
+        password: 'Xgg123456',
         database: 'myweb',
         port: '3306'
     }
